f594e9977baf9b64c48deff7ee768da6fd7f0143
ariazano <radarik@gmail.com>
2023-03-10 12:25:05 +0100

    refresh c00

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,11 +6,13 @@
 /*   By: ariazano <ariazano@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 10:21:25 by ariazano          #+#    #+#             */
-/*   Updated: 2023/03/02 10:21:41 by ariazano         ###   ########.fr       */
+/*   Updated: 2023/03/10 07:53:17 by ariazano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putnbr(int nb);
 
 void	ft_putchar(char c)
 {
@@ -19,19 +21,19 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-		ft_putnbr(nb);
-	}
-	else if (nb == -2147483648)
+	if (nb == -2147483648)
 	{
 		ft_putchar('-');
 		ft_putchar('2');
 		ft_putnbr(147483648);
 	}
-	else if (nb > 10)
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+		ft_putnbr(nb);
+	}
+	else if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
@@ -41,3 +43,21 @@ void	ft_putnbr(int nb)
 		ft_putchar(nb + 48);
 	}
 }
+
+/*
+int	main(void)
+{
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(-42);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(0);
+	ft_putchar('\n');
+}
+
+// cc -Wall -Wextra -Werror ft_putnbr.c -o test; ./test
+*/
